28dd82855b0a4e8071f39e8d3124d5f6ac5137d9
Igor Kupczyński <igor@kupczynski.info>
2020-05-17 18:55:37 +0200

    Log the port on which we start the server

--- go-server/main.go
+++ go-server/main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	addr := ":8080"
+	log.Printf("Starting server on %s\n", addr)
 	handlerFunc := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		now := time.Now()
 		msg := fmt.Sprintf("Hello, world at %s!\n", now.Format("2006-01-02 15:04:05"))
